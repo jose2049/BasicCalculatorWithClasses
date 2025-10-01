ccac2cd9585cb29801b3e2735395fa0231aa7c7d
Jose <g00463862@atu.ie>
2025-10-01 17:10:56 +0100

    adding 2 ints together

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,21 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc=new Scanner(System.in);
+
+        //Create an instance of  Calculator
+        Calculator calc= new Calculator();
+;
+        System.out.println("Simple Calculator -Add two numbers");
+        System.out.println("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.println("Enter a second number: ");
+        int b=sc.nextInt();
+
+        int result=calc.add(a,b);
+        System.out.println("Result: "+ result);
     }
 }
